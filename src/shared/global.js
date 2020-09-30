dec065f93544339d3bb799950205b4438aacc6fa
Michel Bourbeau <michel.bourbeau@akinox.com>
2020-09-30 12:06:11 -0400

    Add Font and CHromatic

--- src/shared/global.js
+++ src/shared/global.js
@@ -1,7 +1,7 @@
 // src/shared/global.js
 
 import { createGlobalStyle, css } from 'styled-components'
-import { color, typography } from './styles'
+// import { color, typography } from './styles'
 
 export const fontUrl =
   'https://fonts.googleapis.com/css?family=Nunito+Sans:400,700,800,900'
